3b1a25f82949aac20da79c9e6042b62a3c5e51d0
edin.sahbaz <edin.sahbaz@hotmail.com>
2021-01-04 18:24:42 +0100

    Test Changes

--- test/stakeServiceController.test.js
+++ test/stakeServiceController.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("./../src/app");
 
-test("Sholud post new ticket to DB", async () => {
+test("Sholud POST new ticket to DB", async () => {
   await request(app)
     .post("/api/v1/stake-limit-service")
     .send("stake=20")
