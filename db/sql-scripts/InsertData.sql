19d79c334d090218fb8e4c49b7cf3191fd355138
edin.sahbaz <edin.sahbaz@hotmail.com>
2021-01-05 21:26:53 +0100

    Updated SQL Scripts

--- db/sql-scripts/InsertData.sql
+++ db/sql-scripts/InsertData.sql
@@ -1,2 +1,9 @@
-INSERT INTO configuration(timeDuration, stakeLimit, hotPercentage, restrictionExpires) 
-VALUES (7200, 100000, 75, 300)
+INSERT INTO
+  configuration(
+    timeDuration,
+    stakeLimit,
+    hotPercentage,
+    restrictionExpires
+  )
+VALUES
+  (300, 1000, 75, 120)
